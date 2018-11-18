13e88db0845f8f02068a87f7cbb7edfcf228d5f9
Raul Zuniga <rzuniga64@gmail.com>
2018-11-17 23:20:12 -0600

    Initial commit

---
+++ src/main/java/com/ws/soap/WsdlfirstwsApplication.java
@@ -0,0 +1,12 @@
+package com.ws.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsdlfirstwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsdlfirstwsApplication.class, args);
+	}
+}
